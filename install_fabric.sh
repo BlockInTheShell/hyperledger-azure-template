86cdf39b07e90817ea726bdd0aad9ec0b584a5a1
César González <cesar@blockintheshell.io>
2018-05-26 18:02:12 +0200

    install_script arguments

--- install_fabric.sh
+++ install_fabric.sh
@@ -16,6 +16,8 @@ npm install -g composer-rest-server
 npm install -g generator-hyperledger-composer
 npm install -g yo
 npm install -g composer-playground
+
+#FABRIC
 mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
 curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.tar.gz
 tar -xvf fabric-dev-servers.tar.gz
