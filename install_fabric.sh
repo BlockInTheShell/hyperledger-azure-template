ff5e17e173f30071c055a883fbc440c18101d8ba
César González <cesar@blockintheshell.io>
2018-05-26 18:54:19 +0200

    install script fix

--- install_fabric.sh
+++ install_fabric.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get -y update
-su $1
+
 
 mkdir ~/install-fabric && cd ~/install-fabric
 
@@ -11,11 +11,11 @@ chmod u+x prereqs-ubuntu.sh
 ./prereqs-ubuntu.sh
 
 #CLI TOOLustom
-npm install -g composer-cli
-npm install -g composer-rest-server
-npm install -g generator-hyperledger-composer
-npm install -g yo
-npm install -g composer-playground
+#npm install -g composer-cli
+#npm install -g composer-rest-server
+#npm install -g generator-hyperledger-composer
+#npm install -g yo
+#npm install -g composer-playground
 
 #FABRIC
 mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
